029914dd35dd6dcf5547a223f7efc3448fba909f
beth-roy <dianaelizabethroy@hotmail.com>
2022-02-02 15:18:06 -0600

    Redux additon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import Header from './components/Header'
-import Home from './components/Home'
+import Header from './components/Header';
+import Home from './components/Home';
+import Detail from './components/Detail';
+import Login from './components/Login'
 
-// import { Counter } from './features/counter/Counter';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+
+<Router>
+  <Header/>
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route path="/detail" element={<Detail/>}/>
+        <Route path="/login" element ={<Login/>}/>
+        
+      </Routes>
+    </Router>
+      
+      
+
     </div>
 
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
 import Movies from './Movies'
+import db from '../firebase';
 
 function Home() {
+
+    useEffect(() => {
+        db.collection("movies").onSnapshot((snapshot)=>{
+            let tempMovies =snapshot.docs.map((doc)=>{
+                return{id: doc.id, ...doc.data()}
+            })
+            console.log(tempMovies)
+        })
+    },[])
   return <Container>
       <ImgSlider/>
       <Viewers />
-      
+      <Movies/>
   </Container>;
 }
 
@@ -18,7 +28,7 @@ const Container = styled.main`
   min-height: calc(100vh - 75px);
   overflow-x: hidden;
   display: block;
-  
+  top:72px
   padding: 0 calc(3.5vw + 5px);
   &:after {
     background: url("/images/home-background.png") center center / cover
